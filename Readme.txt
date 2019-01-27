6c36f4f96851be805aa5e71922d3cafac05da36f
Destiny <andrewbryandolan@gmail.com>
2019-01-27 15:52:31 -0600

    added Readme

---
+++ Readme.txt
@@ -0,0 +1,26 @@
+letters.py 
+-returns lowercase letters not used in a sentence
+-requires user input sentence
+
+animation.py
+-returns graphical representation of particles moving through a room
+-requires user input initial state and speed of particles
+
+
+Built With:
+
+Git Bash
+-git version 2.17.0.windows.1
+
+Python 
+-Python 3.6.4 |Anaconda, Inc.| (default, Jan 16 2018, 10:22:32) [MSC v.1900 64 bi
+t (AMD64)] on win32
+
+Visual Studio Code
+-Version 1.22.2
+Commit 3aeede733d9a3098f7b4bdc1f66b63b0f48c1ef9
+Date 2018-04-12T16:38:45.278Z
+Shell 1.7.12
+Renderer 58.0.3029.110
+Node 7.9.0
+Architecture x64
--- animation.py
+++ animation.py
@@ -1,43 +1,42 @@
+#Import math to use math.ceil to round numbers up to whole number in final loop 
 import math
 
 init = input("What is the initial state? ")
 speed = int(input("What is the speed? "))
 
-#Class object for building the chamber
+#Class object for building the room
 class Animation(object):
-    def __init__(self, chamber):
-        self.chamber = []
+    #Function defines initial state of room
+    def __init__(self, room):
+        self.room = []
         self.particles = []
-        self.len_chamber = len(chamber)
-
-        #Loop to make an empty chamber
-        for i in range(len(chamber)):
-            self.chamber.append(['.'])
-            if chamber[i] is not '.':
-                self.particles.append(Particle(chamber[i],i,self.len_chamber))
-
+        self.len_room = len(room)      
+        for i in range(len(room)):
+            self.room.append(['.'])
+            if room[i] is not '.':
+                self.particles.append(Particle(room[i],i,self.len_room))
+    #Function to build room as particle moves through it
     def animate(self, speed):
         for particle in self.particles:
             particle.move_particle(speed)
-        animation.build_chamber()
-
-    def build_chamber(self):
-        temp_chamber = []
+        animation.build_room()
+    #Function to build a temporary room of '.''s, repaced by 'X''s if a particles occupies a space
+    def build_room(self):
+        temp_room = []
         temp_index = '.'
         result = ''
-
-        # building temp chamber
-        for i in range(len(self.chamber)):
-            temp_chamber.append(['.'])
+        #Build temporary room
+        for i in range(len(self.room)):
+            temp_room.append(['.'])
 
         for particle in self.particles:
             if particle.exists:
-                temp_chamber[particle.position].append(particle)
+                temp_room[particle.position].append(particle)
 
-        # print(temp_chamber)
-        for i in range(len(temp_chamber)):
-            for j in range(len(temp_chamber[i])):
-                if type(temp_chamber[i][j]) is Particle:
+        #Print temporary room
+        for i in range(len(temp_room)):
+            for j in range(len(temp_room[i])):
+                if type(temp_room[i][j]) is Particle:
                     temp_index = 'X'
                 else:
                     temp_index = '.'
@@ -47,32 +46,33 @@ class Animation(object):
 
 #Class object for building the particle
 class Particle(object):
-    def __init__(self, direction, position, len_chamber):
-        # map attributes
+    def __init__(self, direction, position, len_room):
+        #Map attributes
         self.direction = direction
         self.position = position
-        self.len_chamber = len_chamber
+        self.len_room = len_room
         self.exists = True
-
+    #Function for moving the particle, dependent on initial position and speed
     def move_particle(self,speed):
         if self.direction == 'R':
             self.position = self.position + speed
-            if self.position > self.len_chamber - 1:
+            if self.position > self.len_room - 1:
                 self.exists = False
         else:
             self.position = self.position - speed
             if self.position < 0:
                 self.exists = False
 
+#Apply function to display initial state
 animation = Animation(init)
-animation.build_chamber()
-
+animation.build_room()
+#Loop through init as particle moves to print function needed number of times
 if init.find('R') == -1:
     print_range = math.ceil((len(init)-init[::-1].find('L'))/speed)
 elif init[::-1].find('L') == -1:
     print_range = math.ceil((len(init)-init.find('R'))/speed)
 else:
     print_range = math.ceil((len(init)-min(init.find('R'),init[::-1].find('L')))/speed)
-    
+#For every needed space in the initail state, animation is called    
 for i in range(print_range):
     animation.animate(speed)
